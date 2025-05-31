4f18a05464247a286c8efe5d2d861d1fbaa697f8
tims-exe <timonkjohn@gmail.com>
2025-05-31 18:49:09 +0530

    basic chat setup

---
+++ main.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+from typing import Annotated
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from langchain.schema import HumanMessage
+from typing_extensions import TypedDict
+import os
+
+load_dotenv()
+
+llm = init_chat_model(
+    model="llama3-70b-8192",
+    model_provider="groq",
+    api_key=os.getenv("GROQ_API_KEY"),
+    base_url="https://api.groq.com"
+)
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+graph_builder = StateGraph(State)
+
+def chatbot(state: State):
+    response = llm.invoke(state["messages"])
+    return {"messages": state["messages"] + [response]}
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+user_input = input("Enter a msg: ")
+
+state = graph.invoke({
+    "messages": [HumanMessage(content=user_input)]
+})
+
+print("AI:", state["messages"][-1].content)
